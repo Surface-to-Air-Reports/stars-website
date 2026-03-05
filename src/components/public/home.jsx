7ade0bd669aee550db8e3f93ced7645b228cc66b
jawzs <grant.r.elliott@gmail.com>
2026-03-05 13:01:28 -0700

    home page update

--- src/components/public/home.jsx
+++ src/components/public/home.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Box, Button, Typography} from "@mui/joy";
+import {Box, Button, Typography, Tooltip} from "@mui/joy";
 import {useNavigate} from "react-router-dom";
 
 async function loadData() {
@@ -22,8 +22,23 @@ const Page = () => {
         <Box>
             <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem'}}>
                 <Typography level={"h1"} sx={{fontSize: "110px", paddingTop: "10vh"}}>{data}</Typography>
-                <Typography level={"h2"} sx={{width: "500px"}} textAlign={"center"}>Hours of Low-Altitude Flight</Typography>
-                <Typography level={"h3"} sx={{color: "light-gray"}}>Since November 11, 2025</Typography>
+                <Typography level={"h2"} sx={{width: "500px"}} textAlign={"center"}>of Low-Altitude Flight</Typography>
+                <Typography level={"h3"} sx={{color: "light-gray"}}>Updated March 3rd 2026 {' '}
+                    <Tooltip title="Data recording started Novemeber 11th">
+                        <Box
+                        component="span"
+                            sx={{
+                                cursor: 'pointer',
+                                fontSize: '1.25rem',
+                                verticalAlign: 'text-top',
+                                color: 'inherit',
+                                display: 'inline-block',
+                            }}
+                        >
+                        *
+                        </Box>
+                    </Tooltip>
+                     </Typography>
                 <Box sx={{display: 'flex', flexDirection: 'row', gap: '1rem'}}>
                     <Button
                         color={"neutral"}
