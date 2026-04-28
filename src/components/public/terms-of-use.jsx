14b1d4f43bef2520df944fdf58992beaa928b461
jawzs <grant.r.elliott@gmail.com>
2026-04-28 13:51:33 -0600

    fixed tos

--- src/components/public/terms-of-use.jsx
+++ src/components/public/terms-of-use.jsx
@@ -66,9 +66,9 @@ export default function TermsOfUse() {
             5. License and Attribution
           </Typography>
           <Typography sx={{ mb: 2 }}>
-            Unless otherwise stated, datasets made available through this
-            platform are licensed under a Creative Commons Attribution-
-            NonCommercial 4.0 International (CC BY-NC 4.0) license.
+            Unless otherwise stated, all content made available through this
+            platform is licensed under the GNU Affero General Public License
+            v3.0 (AGPL-3.0).
           </Typography>
 
           <List>
